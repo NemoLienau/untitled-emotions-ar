2cc149d9a4660ca9b999722ac872535869263926
NemoLienau <nobodycreating@gmail.com>
2025-07-16 00:01:18 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
+  base: '/untitled-emotions-ar/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
